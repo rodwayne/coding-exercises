15c9fd663a110bd822341046309d8369dffa86ec
rodwayne <ricardo260898@gmail.com>
2024-03-01 18:15:05 -0600

    Solve Find the stray number

---
+++ 8-kyu/Find the stray number/index.js
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/57f609022f4d534f05000024/train/javascript
+
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+const numArr = [1, 1, 2];
+
+function stray(numbers) {
+  const frequency = {};
+
+  numbers.forEach((item) => {
+    if (frequency[item]) {
+      frequency[item]++;
+    } else {
+      frequency[item] = 1;
+    }
+  });
+
+  console.log(frequency);
+
+  for (let key in frequency) {
+    if (frequency[key] === 1) {
+      return parseInt(key);
+    }
+  }
+}
